87664e5e179d2d3179607ee132bed34afedf60b7
AYD2G4 <aydusacg4@gmail.com>
2019-03-05 11:35:56 +0000

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel Hola</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 150;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 156px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+  <br>
+                <br>
+                <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Carne</th>
+      <th scope="col">Nombre</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-info">
+      <th scope="row">200711904</th>
+      <td>William Antonio López Morales</td>
+    </tr>
+    <tr>
+      <th scope="row">201503666</th>
+      <td>Miguel Angel Omar Ruano Roca</td>
+    </tr>
+    <tr class="table-info">
+      <th scope="row">201503936	</th>
+      <td>Elmer Orlando Real Ixcayau</td>
+    </tr>
+    <tr class="table-info">
+      <th scope="row">201504200</th>
+      <td>Denilson Eduardo Argueta Higueros</td>
+    </tr>
+    <tr class="table-info">
+      <th scope="row">201513700</th>
+      <td>Christian Adolfo Real Ixcayau</td>
+    </tr>
+  </tbody>
+</table>  
+
+
+           </div>
+         </div>
+    </body>
+</html>
